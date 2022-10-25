bcfbb2a6c13b89b98510f377be3ae72df6220cb4
diopmorfall <morfall.papydiop99@gmail.com>
2022-10-25 19:23:26 +0200

    Home component deleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import './index.css';
-import Home from './components/Home';
 import Quiz from './components/Quiz';
 
 export default function App() {
     const [isQuizStarted, setIsQuizStarted] = React.useState(false);
+
+    function startQuiz() {
+        setIsQuizStarted((prevIsQuizStarted) => !prevIsQuizStarted);
+    }
+
     return (
-        <main>{isQuizStarted ? <Quiz /> : <Home startQuiz={setIsQuizStarted} />}</main>
+        <main>
+            {isQuizStarted ? (
+                <Quiz />
+            ) : (
+                <section className="home">
+                    <h1>Quizzical</h1>
+                    <p>Test your knowledge</p>
+                    <button className="general-btn" onClick={startQuiz}>
+                        Start quiz
+                    </button>
+                </section>
+            )}
+        </main>
     );
 }
