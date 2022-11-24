363569a3d41ad7b8b86e9f2d9fa326d85863b00f
diopmorfall <morfall.papydiop99@gmail.com>
2022-11-24 21:06:13 +0100

    Logic ofr the selection of the answers complete

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -9,25 +9,47 @@ export default function Quiz() {
         fetch(
         'https://opentdb.com/api.php?amount=5&category=22&difficulty=medium&type=multiple'
         )
-        .then((res) => res.json())
-        .then((data) =>
+        .then(res => res.json())
+        .then(data =>
             setQuestions(() =>
-                data.results.map((question) => new QuestionModel(question))
+                data.results.map(question => new QuestionModel(question))
             )
         );
     }, []);
 
-    const questionElements = questions.map((question) => (
+function selectAnswer(query, selectedAnswer) {
+    setQuestions((prevQuestions) => {
+        return prevQuestions.map(question => {
+            if (question.query === query) {
+                let newAnswers = question.answers.map((answer) => ({
+                ...answer,
+                isSelected: false,
+                }));
+                newAnswers = newAnswers.map(answer => {
+                    if (answer.value === selectedAnswer) {
+                        answer = { ...answer, isSelected: true };
+                    }
+                    return answer;
+                });
+                question.answers = newAnswers;
+            }
+            return question;
+        });
+    });
+}
+
+    const questionElements = questions.map(question => (
         <Question
             key={question.query}
             query={question.query}
         >
-            {question.answers.map((answer) => (
+            {question.answers.map(answer => (
                 <Answer
                     key={answer.value}
                     value={answer.value}
                     isRight={answer.isRight}
                     isSelected={answer.isSelected}
+                    onSelect={() => selectAnswer(question.query, answer.value)}
                 />
             ))}
         </Question>
