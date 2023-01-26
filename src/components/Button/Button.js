ab484a6beb0d979074d2475546b4a94f8a6574b5
diopmorfall <morfall.papydiop99@gmail.com>
2023-01-26 20:25:59 +0100

    Checks for the width of the button based on the media query

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,11 +4,27 @@ import moduleStyles from './Button.module.css';
 export default function Button({ caption, onClick, isDisabled }) {
     let styles;
     if (caption !== 'Start quiz') {
-        styles = {
+        styles = { 
             width: '120px',
             height: '35px',
-            margin: '20px',
+            margin: '20px'
         };
+    
+        if (window.matchMedia('(min-width: 768px)').matches) {
+            styles = {
+                width: '150px',
+                height: '45px',
+                margin: '30px'
+            };
+        }
+    
+        if (window.matchMedia('(min-width: 1000px)').matches) {
+            styles = {
+                width: '185px',
+                height: '55px',
+                margin: '50px'
+            };
+        }
     }
 
     return (
